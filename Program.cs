2b11ce77e29901cc71c18525e58a95f521086bb8
ib-babs <babatundeibrahim426@gmail.com>
2025-01-07 18:14:15 +0100

    Setting connection strings

--- Program.cs
+++ Program.cs
@@ -6,8 +6,8 @@ using Microsoft.Extensions.Azure;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var carSpotLightConnectionString = builder.Configuration.GetConnectionString("SqlServerConnectionString");
-var blobContainerConnectionString = builder.Configuration.GetConnectionString("BlobConnectionString");
+var carSpotLightConnectionString = Environment.GetEnvironmentVariable("SqlServerConnectionString");
+var blobContainerConnectionString = Environment.GetEnvironmentVariable("BlobConnectionString");
 
 builder.Services.AddScoped<ICarRepository, CarRepository>();
 builder.Services.AddAzureClients(azureConfig =>
@@ -25,17 +25,17 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-//// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseMigrationsEndPoint();
-//}
-//else
-//{
-//    app.UseExceptionHandler("/Home/Error");
-//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//    app.UseHsts();
-//}
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    //app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
 //app.Use(async (context, next) =>
 //{
 //    await next();
